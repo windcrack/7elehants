a82022ce6d47a42942bdb4ced3e71db98e201f21
Saha <windcrack@mail.ru>
2019-04-12 15:16:29 +0300

    creat rep file

---
+++ js/sliderOpt.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function sliderRun(slider) {
+        $(slider).slick({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            swipeToSlide: true,
+        })
+    }
+    // sliderRun('.ctaBlock1ImgLeft')
+    // sliderRun('.ctaBlock1ImgRight')
+})

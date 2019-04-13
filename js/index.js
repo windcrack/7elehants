eb16d8fbdf06b4b22429d06d8d7f8ccb92025aef
Saha <windcrack@mail.ru>
2019-04-13 17:10:28 +0300

    add open menu function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-document.addEventListener('DOMContentLoader', () =>{
-    
+document.addEventListener('DOMContentLoaded', () => {
+    const btnMenu = document.querySelector('.headerBurger'),
+          mainMenu = document.querySelector('.menuMain');
+
+    function openBlock (button, block){
+        button.addEventListener('click', (e)=>{
+            // e.preventDefault();
+            block.style.display = 'flex'
+        })
+    }
+    openBlock(btnMenu, mainMenu)
 })

aca7c477bb0840d8d15226140405e74a749ca060
Saha <windcrack@mail.ru>
2019-04-15 17:12:54 +0300

    rework second block and add block reserv

--- js/index.js
+++ js/index.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnMenu = document.querySelector('.headerBurger'),
+          header = document.querySelector('.headerTitle'),
+          btnRezerv = document.querySelector('.rezerv'),
+          modal = document.querySelector('.modal'),
+          btnClose = document.querySelector('.modalBodyClose'),
           mainMenu = document.querySelector('.menuMain');
 
     function openBlock (button, block){
-        button.addEventListener('click', (e)=>{
-            // e.preventDefault();
+        button.addEventListener('click', () => {
             block.style.display = 'flex'
         })
     }
+    function closeBlock (targeting, block){
+        targeting.addEventListener('click', () =>{
+            block.style.display = 'none'
+        })
+    }
     openBlock(btnMenu, mainMenu)
+    openBlock(btnRezerv, modal)
+    closeBlock(header, mainMenu)
+    closeBlock(btnClose, modal);
 })

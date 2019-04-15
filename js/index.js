e9e4e280fe4dc2f3072f269491307e2f8accd8de
Saha <windcrack@mail.ru>
2019-04-16 01:22:11 +0300

    add logo and fixed bugs

--- js/index.js
+++ js/index.js
@@ -1,35 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
     const btnMenu = document.querySelector('.headerBurger'),
-          header = document.querySelector('.headerTitle'),
-          btnRezerv = document.querySelector('.rezerv'),
-          modal = document.querySelector('.modal'),
-          point = $('[href="#descritp"]'),
-          btnClose = document.querySelector('.modalBodyClose'),
-          mainMenu = document.querySelector('.menuMain');
-    
-    function openBlock (button, block){
+        header = document.querySelector('.headerTitle'),
+        btnRezerv = document.querySelector('.rezerv'),
+        btnRezervCenter = document.querySelector('.rezervCenter'),
+        btnRezervRomatic = document.querySelector('.rezervRomantic')
+        modal = document.querySelector('.modal'),
+        arrows = $('[href="#descritp"]'),
+        btnClose = document.querySelector('.modalBodyClose'),
+        blockVisibleMenu = document.querySelector('#descritp'),
+        mainMenu = document.querySelector('.menuMain');
+
+    function openBlock(button, block) {
         button.addEventListener('click', () => {
             block.style.display = 'flex'
         })
     }
-    function closeBlock (targeting, block){
-        targeting.addEventListener('click', () =>{
+
+    function closeBlock(targeting, block) {
+        targeting.addEventListener('click', () => {
             block.style.display = 'none'
         })
     }
-    function scroller (trigg){
+
+    function scrollerAcrossArrow(trigg) {
         trigg.on('click', function (e) {
             let godesc = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(godesc.attr('href')).offset().top
             }, 1000);
             e.preventDefault();
-            // return false;
+            // setTimeout(() => {
+            //     document.querySelector('.menuMain').style.display = 'flex';
+            // }, 1000)
         })
     }
-    scroller(point)
+
+    function scrollMouse(id, block) {
+        postion = $(id).offset().top;
+        $(document).on('scroll', () => {
+            let scroll = $(document).scrollTop();
+            if (scroll > postion) {
+                block.style.display = 'flex'
+            } else {
+                block.style.display = 'none'
+            }
+        })
+    }
+    scrollerAcrossArrow(arrows)
     openBlock(btnMenu, mainMenu)
     openBlock(btnRezerv, modal)
+    openBlock(btnRezervCenter, modal)
+    openBlock(btnRezervRomatic, modal)
     closeBlock(header, mainMenu)
-    closeBlock(btnClose, modal);
+    closeBlock(btnClose, modal)
+    scrollMouse(blockVisibleMenu, mainMenu);
 })

2aa38f299ff57daa77cb3265966f3636068d53fe
Saha <windcrack@mail.ru>
2019-04-19 15:07:46 +0300

    add new page

--- js/index.js
+++ js/index.js
@@ -3,23 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
         header = document.querySelector('.headerTitle'),
         btnRezerv = document.querySelector('.rezerv'),
         btnRezervCenter = document.querySelector('.rezervCenter'),
-        btnRezervRomatic = document.querySelector('.rezervRomantic')
+        btnRezervRomatic = document.querySelector('.rezervRomantic'),
         modal = document.querySelector('.modal'),
         arrows = $('[href="#descritp"]'),
         btnClose = document.querySelector('.modalBodyClose'),
         blockVisibleMenu = document.querySelector('#descritp'),
         mainMenu = document.querySelector('.menuMain');
 
+
     function openBlock(button, block) {
-        button.addEventListener('click', () => {
-            block.style.display = 'flex'
-        })
+        if (button !== null) {
+            button.addEventListener('click', () => {
+                block.style.display = 'flex'
+            })
+        }
     }
 
     function closeBlock(targeting, block) {
-        targeting.addEventListener('click', () => {
-            block.style.display = 'none'
-        })
+        if (targeting !== null) {
+            targeting.addEventListener('click', () => {
+                block.style.display = 'none'
+            })
+        }
+
     }
 
     function scrollerAcrossArrow(trigg) {
@@ -29,27 +35,28 @@ document.addEventListener('DOMContentLoaded', () => {
                 scrollTop: $(godesc.attr('href')).offset().top
             }, 1000);
             e.preventDefault();
-            // setTimeout(() => {
-            //     document.querySelector('.menuMain').style.display = 'flex';
-            // }, 1000)
         })
     }
 
     function scrollMouse(id, block) {
-        postion = $(id).offset().top;
-        $(document).on('scroll', () => {
-            let scroll = $(document).scrollTop();
-            if (scroll > postion) {
-                block.style.display = 'flex'
-            } else {
-                block.style.display = 'none'
-            }
-        })
+        if (id !== null) {
+            postion = $(id).offset().top;
+            $(document).on('scroll', () => {
+                let scroll = $(document).scrollTop();
+                if (scroll > postion) {
+                    block.style.display = 'flex'
+                } else {
+                    block.style.display = 'none'
+                }
+            })
+        }
+
     }
-    $('.switch-btn').click(function(){
+
+    $('.switch-btn').click(function () {
         $(this).toggleClass('switch-on');
     })
-
+    // if (item.querySelector('.detailed')!==null)
     scrollerAcrossArrow(arrows)
     openBlock(btnMenu, mainMenu)
     openBlock(btnRezerv, modal)

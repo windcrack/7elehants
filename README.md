57104a025f49005d3405f144e64b03b4f88231f2
Saha <windcrack@mail.ru>
2019-04-26 14:01:58 +0300

    add script sub menu

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # 7elephants
 
 <ul>
-    <li>Done (adapt - none) - index page</li>
-    <li>Done (adapt - none) - divilery page</li>
-    <li>Done (adapt - none) - Stock page</li>
-    <li>Done (adapt - none) - Cart page</li>
+    <li><span style="color: red">Done</span> (adapt - none) - index page</li>
+    <li><span style="color: red">Done</span> (adapt - none) - divilery page</li>
+    <li><span style="color: red">Done</span> (adapt - none) - Stock page</li>
+    <li><span style="color: red">Done</span> (adapt - none) - Cart page</li>
     <li>Dev  (adapt - none) - Menu page</li>
 </ul>
---
+++ js/scrollActive.js
@@ -0,0 +1,37 @@
+"use strick"
+let loaded = false;
+const menuHead = $('#subMenu a');
+function loadForAncor(classSelect){
+    classSelect.click(function (e) {
+        let target = document.getElementById($(this).attr('href').split('#')[1]);
+        console.log('STATIC Scroll target is valid and ajax already loaded. Calling doScrollAnimate()');
+        doScrollAnimate(target);
+        loaded = true;
+    });
+}
+loadForAncor(menuHead);
+
+function doScrollAnimate($target) {
+    console.log('Animate to ' + $target.id);
+    $('body,html').animate({
+        scrollTop: $target.offsetTop + (-200) + "px"
+    }, 1500);
+}
+if (!loaded && false) {
+    if (window.location.hash) {
+        let target = document.querySelector(window.location.hash);
+    }
+    if (target && (target.classList.contains('ajax') && target.classList.contains('group'))) {
+        console.log('here');
+        let animateTimer = setInterval(function () {
+            if (target.classList.contains('loaded')) {
+                //стопимся
+                clearInterval(animateTimer);
+                console.log('Scroll target is valid and ajax already loaded. Calling doScrollAnimate()');
+                doScrollAnimate(target);
+            } else {
+                console.log('Waiting...');
+            }
+        }, 50);
+    }
+}
